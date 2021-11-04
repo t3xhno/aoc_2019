8b9b253d982bc495705115f6129d56f043081ad2
t3x <t3xhnolainx@gmail.com>
2021-11-04 21:09:41 +0100

    day_1 Refactored solutions

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -3,8 +3,7 @@ use std::{env, fs, error};
 fn main() -> Result<(), Box<dyn error::Error>> {
     let config = Config::new()?;
     let input = fs::read_to_string(&config.file)?;
-    run(&config.solution, &input)?;
-    // println!("{}", calculate_fuel(100756));
+    solve(&config.solution, &input);
     Ok(())
 }
 
@@ -26,35 +25,17 @@ impl Config {
     }
 }
 
-fn run(arg: &str, input: &str) -> Result<(), &'static str> {
-    match arg {
-        "1" => solve1(input),
-        "2" => solve2(input),
-        _ => return Err("Solution is 1 or 2.")
-    };
-    Ok(())
-}
-
 fn calculate_fuel(current: i32) -> i32 {
-    let current = current / 3 - 2;
     match current <= 0 {
         true => 0,
-        _ => current + calculate_fuel(current)
+        _ => current + calculate_fuel(current / 3 - 2)
     }
 }
 
-fn solve1(input: &str) {
-    println!(
-        "{}",
-        input.lines()
-            .fold(0, |acc, line| acc + line.parse::<i32>().unwrap() / 3 - 2)
-    );
-}
-
-fn solve2(input: &str) {
-    println!(
-        "{}",
-        input.lines()
-            .fold(0, |acc, curr| acc + calculate_fuel(curr.parse::<i32>().unwrap()))
-    )
+fn solve(flag: &str, input: &str) {
+    let total_fuel = input.lines().fold(0, |acc, line| acc + match flag {
+        "1" => line.parse::<i32>().unwrap() / 3 - 2,
+        _ => calculate_fuel(line.parse::<i32>().unwrap() / 3 - 2)
+    });
+    println!("{}", total_fuel);
 }
