4d3d3d3a7f7e0a5f15dda4cfadad00b29095cf5d
t3x <t3xhnolainx@gmail.com>
2021-11-04 13:31:28 +0100

    .gitignore

--- day_01/target/debug/day_01.d
+++
@@ -1,1 +0,0 @@
-/home/tex/AOC/2019/day_01/target/debug/day_01: /home/tex/AOC/2019/day_01/src/main.rs
--- day_01/target/debug/deps/day_01-7e0c89204799dddf.d
+++
@@ -1,5 +0,0 @@
-/home/tex/AOC/2019/day_01/target/debug/deps/day_01-7e0c89204799dddf: src/main.rs
-
-/home/tex/AOC/2019/day_01/target/debug/deps/day_01-7e0c89204799dddf.d: src/main.rs
-
-src/main.rs:
